fff33e96ec9b0bb7e4345d1fdb0dd471459ea21d
sonwjnn <hoangsonj4f@gmail.com>
2024-01-11 18:33:31 +0700

    test to fix bug production

--- src/app/(browse)/[username]/error.tsx
+++ src/app/(browse)/[username]/error.tsx
@@ -2,11 +2,17 @@
 
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { useEffect } from 'react'
 
-const ErrorPage = () => {
+const ErrorPage = ({ error }: { error: any }) => {
+  useEffect(() => {
+    console.log(error)
+  }, [error])
+
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4 text-muted-foreground">
       <p>Something went wrong</p>
+      <p>{`${error}`}</p>
       <Button variant="secondary" asChild>
         <Link href="/">Go back home</Link>
       </Button>
