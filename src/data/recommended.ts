3e57793bd27c7dcd1f3e9435035daa64e18d3c99
sonwjnn <hoangsonj4f@gmail.com>
2024-01-11 09:28:05 +0700

    test to fix bug production

--- src/data/recommended.ts
+++ src/data/recommended.ts
@@ -4,6 +4,10 @@ import { getSelf } from './auth'
 
 export const getRecommended = async () => {
   try {
+    const self = await getSelf()
+
+    const userId = self?.id
+
     return []
   } catch {
     return []
