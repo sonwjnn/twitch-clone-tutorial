c57c3944ecffef30f4cf11814ccbfdba9520c9d4
sonwjnn <hoangsonj4f@gmail.com>
2024-01-11 09:02:24 +0700

    test to fix bug production

--- src/data/recommended.ts
+++ src/data/recommended.ts
@@ -3,83 +3,85 @@ import { db } from '@/lib/db'
 import { getSelf } from './auth'
 
 export const getRecommended = async () => {
+  let userId
+
   try {
     const self = await getSelf()
-
-    let users = []
-
-    if (self?.id) {
-      //Find users that current (self) users are not following and are not blocked
-      users = await db.user.findMany({
-        where: {
-          AND: [
-            {
-              NOT: {
-                id: self.id,
-              },
-            },
-            {
-              NOT: {
-                followedBy: {
-                  some: {
-                    followerId: self.id,
-                  },
-                },
-              },
-            },
-            {
-              NOT: {
-                blocking: {
-                  some: {
-                    blockedId: self.id,
-                  },
-                },
-              },
-            },
-          ],
-        },
-        include: {
-          stream: {
-            select: {
-              isLive: true,
-            },
-          },
-        },
-        orderBy: [
-          {
-            stream: {
-              isLive: 'desc',
-            },
-          },
-          {
-            createdAt: 'desc',
-          },
-        ],
-      })
-    } else {
-      users = await db.user.findMany({
-        include: {
-          stream: {
-            select: {
-              isLive: true,
-            },
-          },
-        },
-        orderBy: [
-          {
-            stream: {
-              isLive: 'desc',
-            },
-          },
-          {
-            createdAt: 'desc',
-          },
-        ],
-      })
-    }
-
-    return users
+    userId = self?.id
   } catch {
-    return []
+    userId = null
   }
+
+  let users = []
+
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        AND: [
+          {
+            NOT: {
+              id: userId,
+            },
+          },
+          {
+            NOT: {
+              followedBy: {
+                some: {
+                  followerId: userId,
+                },
+              },
+            },
+          },
+          {
+            NOT: {
+              blocking: {
+                some: {
+                  blockedId: userId,
+                },
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        stream: {
+          select: {
+            isLive: true,
+          },
+        },
+      },
+      orderBy: [
+        {
+          stream: {
+            isLive: 'desc',
+          },
+        },
+        {
+          createdAt: 'desc',
+        },
+      ],
+    })
+  } else {
+    users = await db.user.findMany({
+      include: {
+        stream: {
+          select: {
+            isLive: true,
+          },
+        },
+      },
+      orderBy: [
+        {
+          stream: {
+            isLive: 'desc',
+          },
+        },
+        {
+          createdAt: 'desc',
+        },
+      ],
+    })
+  }
+
+  return users
 }
