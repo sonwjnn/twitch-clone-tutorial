be106e2b217a7af8ad3f5ee0d181723dd96150a5
sonwjnn <hoangsonj4f@gmail.com>
2024-01-14 15:10:57 +0700

    test to fix bug production

--- src/app/(dashboard)/u/[username]/layout.tsx
+++ src/app/(dashboard)/u/[username]/layout.tsx
@@ -23,9 +23,9 @@ const CreatorLayout = async ({ params, children }: CreatorLayoutProps) => {
     <>
       <Navbar />
       <div className="flex h-full pt-20">
-        {/* <Suspese fallback={'Loading...'}> */}
-        <Sidebar />
-        {/* </Suspese> */}
+        <Suspense fallback={'...Loading'}>
+          <Sidebar />
+        </Suspense>
         <Container>{children}</Container>
       </div>
     </>
