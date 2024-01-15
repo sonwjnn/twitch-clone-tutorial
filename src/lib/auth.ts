68ebe58c46a09c2d58ba68afbb1f7390d8c50d7e
sonwjnn <hoangsonj4f@gmail.com>
2024-01-15 20:24:36 +0700

    test to fix bug production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,24 @@
 import { auth } from '@/auth'
 
+const testUser = {
+  name: 'sonwin111',
+  email: 'nguyenlehoangson2106@gmail.com',
+  image: null,
+  id: 'clr7mclbe000012d77p333jre',
+  role: 'USER',
+  isTwoFactorEnabled: false,
+  isOAuth: false,
+  stream: { name: "sonwin111's stream", isLive: false },
+}
+
 export const currentUser = async () => {
   const session = await auth()
-
-  console.log(session?.user)
-  return session?.user
+  // return session?.user
+  return testUser
 }
 
 export const currentRole = async () => {
   const session = await auth()
-  return session?.user?.role
+  return testUser.role
+  // return session?.user?.role
 }
